44604eedf74d2597f32ee4a7cc30f2add3c76353
Gams <gams.basallo@gmail.com>
2023-03-02 14:24:11 +0800

    Add user seeder class

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            'email' => 'admin@test.com',
+            'password' => 'Developer123!'
+        ];
+
+        User::create($admin);
+    }
+}
