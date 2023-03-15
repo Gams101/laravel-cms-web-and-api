903ffc9bde2c01cf9049880efae3bd60ce83d4a6
Gammalil Basallo <gams.basallo@gmail.com>
2023-03-15 08:48:02 +0800

    Update user seeder for role assignment

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,12 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $admin = [
-            'email' => 'admin@test.com',
-            'password' => bcrypt('Developer123!')
-        ];
 
-        User::factory()->create($admin);
+        /** Roles */
+        $adminRole = Role::create(['name' => 'admin']);
 
+        /** User */
+        $adminUser = User::factory()
+            ->create([
+                'email' => 'admin@test.com',
+                'password' => bcrypt('Developer123!')
+            ]);
+
+        $adminUser->assignRole($adminRole);
     }
 }
